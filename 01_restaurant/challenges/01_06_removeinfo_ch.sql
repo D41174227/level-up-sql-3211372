2e3cc32aeedc3d39b55aeb18addda5f07364badc
Derrick Warner <derrick.warner@mail.waldenu.edu>
2024-11-12 18:43:52 +0000

    01_06

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,12 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT *
+FROM Reservations r
+join customers c on r.customerid = c.customerid
+where c.firstname = 'Norby'
+and r.date > '2022-07-04'
+
+delete from Reservations
+where ReservationID = 2000
