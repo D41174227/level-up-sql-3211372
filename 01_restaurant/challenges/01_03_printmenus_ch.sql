1e54389cd4820b7f0e8d37e5eef76bdcd4023ae1
Derrick Warner <derrick.warner@mail.waldenu.edu>
2024-11-12 15:17:06 +0000

    01_03

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,14 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+SELECT Name, Price
+FROM Dishes
+ORDER BY Price;
+
+SELECT *
+FROM Dishes
+WHERE Type IN ('Appetizer','Beverage');
+
+SELECT *
+FROM Dishes
+WHERE Type != 'Beverage';
