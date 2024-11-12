e24ea34a8d4f7a2db935290ba17e0714f6432cdd
Derrick Warner <derrick.warner@mail.waldenu.edu>
2024-11-12 18:46:47 +0000

    01_07

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,11 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+insert into AnniversaryInvitationResponses
+select customers.customerid, 3
+from customers
+where email = 'atapley2j@kinetecoinc.com'
+
+select *
+from AnniversaryInvitationResponses
